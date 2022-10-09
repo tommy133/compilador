75294445af50c299cb3edc1350f58af2e1451b26
Tomeu Estrany <tomestrany@gmail.com>
2022-10-09 18:00:09 +0200

    feat:data types

---
+++ src/front/data_types/Stack.java
@@ -0,0 +1,52 @@
+package front.data_types;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+
+    private List<T> elements;
+
+    public Stack(){
+        elements = new ArrayList<>();
+    }
+
+
+    public T Peek() {
+        if (elements.isEmpty()) {
+            return null;
+        }
+        return elements.get(elements.size() - 1);
+    }
+
+    public T Pop() {
+        if (elements.isEmpty()) {
+            return null;
+        }
+        T top = elements.get(elements.size() - 1);
+        elements.remove(elements.size() - 1);
+        return top;
+    }
+
+    public void Push(T element) {
+        elements.add(element);
+    }
+
+    public T Get(int i){
+        return elements.get(i);
+    }
+
+    public int Size() {
+        return elements.size();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "Stack [elements=" + elements + "]";
+    }
+
+}
---
+++ src/front/data_types/Types.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package front.data_types;
+
+
+public enum Types {
+    MAINPROG, VARIABLE, CONSTANT, FUNCTION, METHOD
+}
