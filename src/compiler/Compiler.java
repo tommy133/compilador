3f33ed78496061cd6ae3e5229219d2a789cdc079
Tomeu Estrany <tomestrany@gmail.com>
2022-12-28 10:38:04 +0100

    feat:Compiler main target

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -6,7 +6,11 @@
 package compiler;
 
 
+import back.generator.EnsamblerCode;
+import back.generator.ThreeAddressCodeSintetic;
+import front.parser.Parser;
 import front.scanner.LexicalScanner;
+import java_cup.runtime.ComplexSymbolFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -15,10 +19,12 @@ import java.util.logging.Logger;
 
 
 public class Compiler {
+    private static LexicalScanner scanner = null;
+    private static ThreeAddressCodeSintetic tac;
+    private static EnsamblerCode ens;
 
     public static void main(String[] args) {
 
-        LexicalScanner scanner = null;
 
 
         try {
@@ -28,12 +34,21 @@ public class Compiler {
             }
 
             clean("Tokens.txt");
+            clean("codiIntermitg.txt");
+            clean("Errors.txt");
+            clean("codi_ensamblador.X68");
 
             scanner = new LexicalScanner(new FileReader(args[0]));
-            //Parser parser = new Parser(scanner, new ComplexSymbolFactory());
-            //parser.parse();
+            Parser parser = new Parser(scanner, new ComplexSymbolFactory());
+            parser.parse();
+            if (parser != null) {
+                System.out.println(">> OK Parsing ["+args[0]+"]");
+                generateBack("files_output/");
 
-            System.out.println("COMPILACIÓ COMPLETADA AMB ÈXIT");
+                System.out.println("COMPILACIÓ COMPLETADA AMB ÈXIT");
+            } else {
+                System.out.println(">>>>>> Errors detectats");
+            }
 
         } catch (FileNotFoundException ex) {
             System.out.println("Fitxer no trobat!!");
@@ -56,6 +71,30 @@ public class Compiler {
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void generateBack(String path) throws IOException {
+        tac = new ThreeAddressCodeSintetic();
+        ens = new EnsamblerCode(tac);
+
+        ens.generate();
+        writeFile(path + "codi_ensamblador.X68", ens.getCode());
+        //this.opt.optimizar();
+        //writeFile(path + "codi_intermig_optimizado.txt", tac.viewCode());
+        //ens.generate();
+        //writeFile(path + "codi_ensamblador_optimizado.X68", ens.getCode());
+    }
+
+    private static void writeFile(String canonicalFilename, String text)
+            throws IOException
+    {
+        Writer out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(canonicalFilename), "UTF-8"));
+        try {
+            out.write(text);
+        } finally {
+            out.close();
         }
     }
 }
