b8f941963dc1bd8e0dc545149f3bf261fdfc798a
Tomeu Estrany <tomestrany@gmail.com>
2023-01-26 22:45:57 +0100

    feat:Operation missing in version control

---
+++ src/back/data_structures/Operation.java
@@ -0,0 +1,26 @@
+package back.data_structures;
+
+public enum Operation {
+    ASIGNA,
+    SUMA,
+    RESTA,
+    DIVISION,
+    MULTIPLICACION,
+    AND,
+    OR,
+    GOTO,
+    SKIP,
+    IF,
+    IFIGUAL,
+    IFMAYOR,
+    IFMENOR,
+    IFMAYORIGUAL,
+    IFMENORIGUAL,
+    IFDIFERENTE,
+    CALL,
+    RTN,
+    PMB,
+    PARAM_S,
+    PARAM_C,
+    DESPLAZAR_BITS
+}
